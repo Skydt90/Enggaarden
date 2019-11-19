b34accd99c94219cbb5017e4d8667d262005de26
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2019-11-19 13:56:09 +0100

    added validation for address fields

--- app/Http/Requests/CreateMemberRequest.php
+++ app/Http/Requests/CreateMemberRequest.php
@@ -29,7 +29,10 @@ class CreateMemberRequest extends FormRequest
             'first_name' => 'required|max:50',
             'email' => 'required|max:100|email|unique:members,email',
             'phone_number' => 'required|numeric|min:10000000|max:99999999',
-            'member_type' => Rule::in(Member::MEMBER_TYPES)
+            'member_type' => Rule::in(Member::MEMBER_TYPES),
+            'street_name' => 'max:50',
+            'zip_code' => 'max:4',
+            'city' => 'max:30'
         ];
     }
 
@@ -42,7 +45,10 @@ class CreateMemberRequest extends FormRequest
             'phone_number.required' => 'Mobilnummer er påkrævet',
             'phone_number.max' => 'Mobilnummer er for langt',
             'phone_number.min' => 'Mobilnummer er for kort',
-            'member_type.in' => 'Medlemstypen er ugyldig'
+            'member_type.in' => 'Medlemstypen er ugyldig',
+            'zip_code.max' => 'Postnummeret er ugyldigt',
+            'street_name.max' => 'Vejnavnet er for langt',
+            'city.max' => 'Bynavnet er for langt'
         ];
     }
 }
