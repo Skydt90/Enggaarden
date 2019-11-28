6b5ba466c683c61e6bebf8c73eef66dd044eda08
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2019-11-28 12:47:32 +0100

    add corect titles to error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Not Found</title>
+        <title>Unauthenticated</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
