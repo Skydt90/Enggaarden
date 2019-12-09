e41c514ad5e0707e4ceb6a9d533d70f0b722c4f0
Skydt <Christian_skydt@hotmail.com>
2019-12-09 16:52:22 +0100

    print added to index

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -11,9 +11,10 @@
         @toast @endtoast
         <div class="row">
             <button type="button" id="register-button" data-toggle="modal" data-target="#register-modal" class="btn btn-sm btn-success"><i class="fas fa-user-plus"></i> Opret Medlem</button>
-            <button type="button" id="register-company-button" data-toggle="modal" data-target="#register-company-modal" class="btn btn-sm btn-success ml-1"><i class="far fa-building"></i> Opret Firma</button> 
+            <button type="button" id="register-company-button" data-toggle="modal" data-target="#register-company-modal" class="btn btn-sm btn-success ml-1"><i class="far fa-building"></i> Opret Firma</button>
+            <button type="button" id="print" onClick="window.print();" class="btn btn-sm btn-primary ml-auto"><i class="fas fa-print"></i> Udskriv Medlemmer</button>
         </div>
-
+        
         <div class="row mt-1">
             <input type="text" id="search" onkeyup="searchTable('members')" placeholder="Søg efter navn..">  
             @amount(['urlID' => 'member', 'amount' => $amount]) 
